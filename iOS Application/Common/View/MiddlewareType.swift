2058a93f82e1a9618d7387b10c67f087cea3ca73
Basem Emara <contact@basememara.com>
2019-11-27 02:34:38 -0500

    Add more cccomments and extend reducer protocol

--- iOS Application/Common/View/MiddlewareType.swift
+++ iOS Application/Common/View/MiddlewareType.swift
@@ -5,6 +5,7 @@
 //  Created by Basem Emara on 2019-11-26.
 //
 
+/// The middleware that passively executes tasks based on any action before it it sent to a reducer.
 protocol MiddlewareType {
     func execute(on action: ActionType)
 }
--- iOS Application/Common/View/ReducerType.swift
+++ iOS Application/Common/View/ReducerType.swift
@@ -6,4 +6,12 @@
 //
 
 /// The reducer to dispatch an action to state for the scene.
-protocol ReducerType {}
+protocol ReducerType {
+    associatedtype Action = ActionType
+    
+    /// Performs the logic and mutates the store.
+    /// - Parameters:
+    ///   - store: The global store for the application
+    ///   - action: The action to perform on the store.
+    func apply(_ store: AppStore, _ action: Action)
+}
